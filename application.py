e19867e4c162e4c62dfc1eb13b9bc070011bfabd
Pathak <pratik.pathak@amecfw.com>
2020-09-10 22:27:15 -0400

    User option added

--- application.py
+++ application.py
@@ -6,6 +6,10 @@ application = Flask(__name__)
 def index():
     return 'Hello World!'
 
+@application.route('/<anyname>')
+def any_name(anyname):
+    return f'Hello {anyname}'
+
 if __name__ == '__main__':
     application.debug = True
     application.run()
